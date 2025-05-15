85335aa45a2b5caa1121f755f3795a866e693c3d
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-05-15 17:11:02 +0200

    feat(auth): update authentication routes

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { loggedInGuard } from '../core/guards/logged-in.guard';
 
 export const AUTH_ROUTES: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate: [loggedInGuard]
   },
   {
     path: '',
