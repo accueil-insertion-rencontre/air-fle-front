7cebc88664cf59d76dee1e778f04bc6f5f4d6594
Gabriel Luthun <gabriel.luthun@gmail.com>
2025-05-22 15:10:12 +0200

    feat(module): add student module stucture

---
+++ src/app/dashboard/pages/apprenants/apprenants.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApprenantsRoutingModule } from './apprenants-routing.module';
+import { ApprenantsComponent } from './apprenants.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ApprenantsRoutingModule,
+    ApprenantsComponent
+  ]
+})
+export class ApprenantsModule { } 
