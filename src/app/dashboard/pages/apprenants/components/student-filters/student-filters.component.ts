0f73b1d663947253c72194f4765d01b76b31bb0b
Gabriel Luthun <gabriel.luthun@gmail.com>
2025-05-22 14:58:53 +0200

    feat(components): add ts for student-filters component

---
+++ src/app/dashboard/pages/apprenants/components/student-filters/student-filters.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { StudentFilters } from '../../models/student.model';
+import { Level } from '../../models/level.model';
+import { Status } from '../../models/status.model';
+import { Nationality } from '../../models/nationality.model';
+
+@Component({
+  selector: 'app-student-filters',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './student-filters.component.html',
+  styleUrls: ['./student-filters.component.scss']
+})
+export class StudentFiltersComponent {
+  @Input() levels: Level[] = [];
+  @Input() statuses: Status[] = [];
+  @Input() nationalities: Nationality[] = [];
+  @Output() filtersChange = new EventEmitter<StudentFilters>();
+
+  filters: StudentFilters = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    level: '',
+    status: '',
+    nationality: ''
+  };
+
+  onFiltersChange(): void {
+    this.filtersChange.emit(this.filters);
+  }
+
+  clearFilters(): void {
+    this.filters = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      level: '',
+      status: '',
+      nationality: ''
+    };
+    this.filtersChange.emit(this.filters);
+  }
+} 
