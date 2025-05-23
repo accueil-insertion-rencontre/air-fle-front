6ad37e1c67b7aec3f681055705d5b195287642cb
UnknOownU <149084644+UnknOownU@users.noreply.github.com>
2025-05-23 02:54:44 +0200

    refactor(structure): move user module to dashboard/pages directory

---
+++ src/app/dashboard/pages/users/users.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { UsersRoutingModule } from './users-routing.module';
+import { UserListComponent } from './user-list/user-list.component';
+
+@NgModule({
+  declarations: [
+    // Les composants standalone ne sont pas déclarés ici
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule,
+    UsersRoutingModule,
+    UserListComponent
+  ]
+})
+export class UsersModule { } 
